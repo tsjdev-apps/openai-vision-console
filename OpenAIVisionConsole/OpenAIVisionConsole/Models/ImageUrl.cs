85f7fb122f2a61390e9573a57202b73c66891149
Sebastian Jensen <apps@tsjdev-apps.de>
2023-11-16 19:56:53 +0100

    Adjust records

--- OpenAIVisionConsole/OpenAIVisionConsole/Models/ImageUrl.cs
+++ OpenAIVisionConsole/OpenAIVisionConsole/Models/ImageUrl.cs
@@ -6,12 +6,7 @@ namespace OpenAIVisionConsole.Models;
 ///     Represents an image URL.
 /// </summary>
 /// <param name="Url"> The URL of the image. </param>
+/// <param name="Detail"> The detail of the image. </param>
 internal record ImageUrl(
-    [property: JsonPropertyName("url")] string Url)
-{
-    /// <summary>
-    ///     The detail of the image.
-    /// </summary>
-    [JsonPropertyName("detail")]
-    public string? Detail { get; } = "low";
-}
+    [property: JsonPropertyName("url")] string Url,
+    [property: JsonPropertyName("detail")] string? Detail = "low");
